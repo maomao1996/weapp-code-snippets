a8b9c5ab04736d3e89aa73336637e4715ad25b35
maomao1996 <1714487678@qq.com>
2022-01-05 19:29:49 +0800

    feat(wxs):  添加 get 方法

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,24 @@
-Page({})
+Page({
+  data: {
+    modules: [
+      {
+        name: '自定义组件',
+        data: [
+          {
+            url: '/pages/popup/index',
+            name: 'Popup 弹出层'
+          }
+        ]
+      },
+      {
+        name: 'WXS 方法',
+        data: [
+          {
+            url: '/wxsPackage/pages/get/index',
+            name: '在 wxml 中使用 lodash.get'
+          }
+        ]
+      }
+    ]
+  }
+})
